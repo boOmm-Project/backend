30498fb52d510b7765e3bbdf9606f56d84f9947c
Gyuwon-Jung <licmm0520@gmail.com>
2025-11-19 20:09:04 +0900

    initialize project

---
+++ src/main/java/com/nuclear/boomm/BoOmmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nuclear.boomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoOmmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoOmmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuclear/boomm/BoOmmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuclear.boomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoOmmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
