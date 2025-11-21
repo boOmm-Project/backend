3240af5a25df6417ceed1ad62ed895ac90e52cb3
chaewookim <chaewookim2877@gmail.com>
2025-11-21 18:56:52 +0900

    [fix] #14 product entity에 annotation 추가

--- src/main/java/com/nuclear/boomm/product/product/Product.java
+++ src/main/java/com/nuclear/boomm/product/product/Product.java
@@ -2,26 +2,39 @@ package com.nuclear.boomm.product.product;
 
 import com.nuclear.boomm.common.BaseEntity;
 import com.nuclear.boomm.product.enums.FeedbackStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
+@Table(name = "product")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Product extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
 
+    @Enumerated(EnumType.STRING)
+    private FeedbackStatus status;
+
     private String productName;
-    private Long category;                    // Product_Category 테이블의 id
+    private Long category;  // Product_Category 테이블의 id
     private String targetCustomer;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private String salesChannel;
-    @Enumerated(EnumType.STRING)
-    private FeedbackStatus status;
 }
