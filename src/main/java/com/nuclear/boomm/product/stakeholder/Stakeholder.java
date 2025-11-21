1743e54995d52e3ff910b9875d025c40c80cf733
chaewookim <chaewookim2877@gmail.com>
2025-11-21 19:10:41 +0900

    [feat] #9 stakeholder entity 구현

---
+++ src/main/java/com/nuclear/boomm/product/stakeholder/Stakeholder.java
@@ -0,0 +1,30 @@
+package com.nuclear.boomm.product.stakeholder;
+
+import com.nuclear.boomm.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "stakeholder")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Stakeholder extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long stakeholderId;
+
+    private Long userId;        // user 테이블 pk 참조
+    private Long productId;     // product 테이블 pk 참조
+    private Long feedbackId;    // feedback 테이블 pk 참조
+}
