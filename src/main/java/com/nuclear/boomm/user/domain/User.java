7f5af313350b209c4d232d434cd0b4e09a6e06f5
Ongsaem0 <tkdwnsghqkr1@gmail.com>
2025-11-24 20:49:51 +0900

    [feat] #42 user엔티티 및 성별 Enum 구현

---
+++ src/main/java/com/nuclear/boomm/user/domain/User.java
@@ -0,0 +1,53 @@
+package com.nuclear.boomm.user.domain;
+
+import com.nuclear.boomm.user.enums.Gender;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name="user")
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String idNum;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String zipCode;
+
+    @Column(nullable = false)
+    private String job;
+
+    @Column(nullable = false)
+    private boolean wedding;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String companyName;
+
+
+}
---
+++ src/main/java/com/nuclear/boomm/user/enums/Gender.java
@@ -0,0 +1,6 @@
+package com.nuclear.boomm.user.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+}
