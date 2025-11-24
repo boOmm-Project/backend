6d6af405f036fcc4243cc7ead979f204f8d259fb
Ongsaem0 <tkdwnsghqkr1@gmail.com>
2025-11-24 21:13:10 +0900

    [feat] #29 SecurityInfo 엔티티 구현

--- src/main/java/com/nuclear/boomm/user/domain/SecurityInfo.java
+++ src/main/java/com/nuclear/boomm/user/domain/SecurityInfo.java
@@ -1,4 +1,25 @@
 package com.nuclear.boomm.user.domain;
 
-public class SecurityInfo {
+import com.nuclear.boomm.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name="security_info")
+@NoArgsConstructor
+public class SecurityInfo extends BaseEntity {
+
+    @Id
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private boolean marketingAgreement;
+
+    @Column(nullable = false)
+    private String personalInfo;
+
+
 }
