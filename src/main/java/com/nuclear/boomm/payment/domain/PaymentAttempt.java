1a7eec9de4633e4d43b8ae0ab475a1e14ea98a73
yoocy0 <yoocy0328@gmail.com>
2025-11-25 21:29:59 +0900

    [feat] #35 PaymentAttempt 구엔티티 및 FailureMessage PaymentStatus enum 구현

---
+++ src/main/java/com/nuclear/boomm/payment/domain/PaymentAttempt.java
@@ -0,0 +1,48 @@
+package com.nuclear.boomm.payment.domain;
+
+import com.nuclear.boomm.common.BaseEntity;
+import com.nuclear.boomm.payment.enums.FailureMessage;
+import com.nuclear.boomm.payment.enums.PaymentStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payment_attempt")
+@Getter
+@NoArgsConstructor
+public class PaymentAttempt extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long PaymentAttemptId;
+
+    @Column(nullable = false)
+    private String policyNumber;
+
+    @Column(nullable = false)
+    private LocalDateTime paymentAttemptedAt;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private double paymentAmount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private FailureMessage failureMessage;
+
+    @Column(nullable = false)
+    private Long paymentId;
+}
