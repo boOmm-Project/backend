b1481790319746bc7c5339f3782aa417e266c5d3
yoocy0 <yoocy0328@gmail.com>
2025-11-25 19:23:32 +0900

    [feat] #32 CostomerDocument 엔티티 및 e및 티 및 DocumentType FileType ProcessingStatus enum 구현

---
+++ src/main/java/com/nuclear/boomm/contract/domain/CustomerDocument.java
@@ -0,0 +1,48 @@
+package com.nuclear.boomm.contract.domain;
+
+import com.nuclear.boomm.common.BaseEntity;
+import com.nuclear.boomm.contract.enums.DocumentType;
+import com.nuclear.boomm.contract.enums.FileType;
+import com.nuclear.boomm.contract.enums.ProcessingStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name="customer_document")
+@NoArgsConstructor
+public class CustomerDocument extends BaseEntity {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private long userId;   // fk
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DocumentType documentType;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private FileType fileType;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Column(columnDefinition = "text")
+    private String documentRemark;
+
+    private ProcessingStatus processingStatus;
+}
