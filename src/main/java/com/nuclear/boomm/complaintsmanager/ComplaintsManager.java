6b575f613e94c5f52647f6bad0a6c7f18479ae75
Ongsaem0 <tkdwnsghqkr1@gmail.com>
2025-11-26 16:32:41 +0900

    [feat] #30 ComplaintsManager 엔티티 구현

--- src/main/java/com/nuclear/boomm/complaintsmanager/ComplaintsManager.java
+++ src/main/java/com/nuclear/boomm/complaintsmanager/ComplaintsManager.java
@@ -1,4 +1,40 @@
 package com.nuclear.boomm.complaintsmanager;
 
-public class ComplaintsManager {
+import com.nuclear.boomm.common.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ComplaintsManager extends BaseEntity {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long consultationId;
+
+    @Column(nullable = false)
+    private Long managerId;
+
+    @Column(nullable = false)
+    private String consultationMemo;
+
+
+    @Builder
+    public ComplaintsManager(Long consultationId, Long managerId, String consultationMemo) {
+        this.consultationId = consultationId;
+        this.managerId = managerId;
+        this.consultationMemo = consultationMemo;
+    }
+
 }
