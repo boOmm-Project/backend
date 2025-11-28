a245a0bb04f39429da14c963718a43f2113cddc7
Gyuwon-Jung <licmm0520@gmail.com>
2025-11-28 20:00:50 +0900

    [feat] #20 사고 보상 공통 엔티티 구현

---
+++ src/main/java/com/nuclear/boomm/caraccident/domain/GeneralAccidentAssessmentEntity.java
@@ -0,0 +1,55 @@
+package com.nuclear.boomm.caraccident.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "accidentType")
+public abstract class GeneralAccidentAssessmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long assessmentManagerId; // 심사 담당 매니저 ID
+
+    @Column
+    private Long intakeManagerId; // 접수 담당자 ID
+
+    @Column
+    private Long accidentIntakeId; // 사고 접수 ID
+
+    @Column
+    private Long insureClaimFileId; // 보험금 청구서 파일
+
+    @Column
+    private Long proofFileId; // 손해액을 증명하는 서류
+
+    @Builder
+    public GeneralAccidentAssessmentEntity(Long intakeManagerId, Long accidentIntakeId, Long insureClaimFileId, Long proofFileId) {
+        this.intakeManagerId = intakeManagerId;
+        this.accidentIntakeId = accidentIntakeId;
+        this.insureClaimFileId = insureClaimFileId;
+        this.proofFileId = proofFileId;
+    }
+
+    // 심사 담당자 할당
+    public void assignManagerId(Long assessmentManagerId) {
+        this.assessmentManagerId = assessmentManagerId;
+    }
+
+}
