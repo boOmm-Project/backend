6bddd4ffc78472ceb5eb2314c1c1336dd8d33d64
Gyuwon-Jung <licmm0520@gmail.com>
2025-11-30 23:47:28 +0900

    [refactor] #63 접수 엔티티 주석 추가 및 LongText에서 TEXT로 바꿈

--- src/main/java/com/nuclear/boomm/caraccident/domain/AccidentIntakeEntity.java
+++ src/main/java/com/nuclear/boomm/caraccident/domain/AccidentIntakeEntity.java
@@ -16,7 +16,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -28,42 +28,42 @@ public class AccidentIntakeEntity extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(columnDefinition = "text", nullable = false)
-    private String accidentDescription;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String accidentDescription; // 사고 사항
 
-    @Column(columnDefinition = "text", nullable = false)
-    private String damageDescription;
-
-    @Column(nullable = false)
-    private LocalDate incidentDate;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String damageDescription; // 피해 사항
 
     @Column(nullable = false)
-    private String carNumber;
+    private LocalDateTime incidentDate; // 사고 일자
 
     @Column(nullable = false)
-    private Long insuredPersonId;
+    private String carNumber; // 차량번호
 
     @Column(nullable = false)
-    private String insuranceClaimPersonName;
+    private Long insuredPersonId; // 피보험자 아이디
 
     @Column(nullable = false)
-    private String insuranceClaimPersonEmail;
+    private String insuranceClaimPersonName; // 보험금청구 접수자
+
+    @Column(nullable = false)
+    private String insuranceClaimPersonEmail; // 보험금청구 접수자 이메일
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private BankEnum insurancePaymentBank;
+    private BankEnum insurancePaymentBank; // 보험금 지금 계좌 은행
 
     @Column(nullable = false)
-    private String insurancePaymentBankAccount;
+    private String insurancePaymentBankAccount; // 보험금 지급 계좌
 
     @Enumerated(EnumType.STRING)
-    private InsuranceClaimStatus intakeStatus;
+    private InsuranceClaimStatus intakeStatus; // 진행 상태
 
     @Column
-    private Long intakeManagerId;
+    private Long intakeManagerId; // 접수 담당자 아이디
 
     @Builder
-    public AccidentIntakeEntity(String accidentDescription, String damageDescription, LocalDate incidentDate,
+    public AccidentIntakeEntity(String accidentDescription, String damageDescription, LocalDateTime incidentDate,
                                 String carNumber, Long insuredPersonId, String insuranceClaimPersonName,
                                 String insuranceClaimPersonEmail, BankEnum insurancePaymentBank, String insurancePaymentBankAccount
                                 ) {
