927b745d054d9dc0a144b7eb8ffc0c74fe5397bf
chaewookim <chaewookim2877@gmail.com>
2025-12-24 09:32:12 +0900

    [feat] #77 ErrorCode 추가
    출시된 상품을 삭제하려 했을 때의 오류 코드 추가

--- src/main/java/com/nuclear/boomm/product/error/ErrorCode.java
+++ src/main/java/com/nuclear/boomm/product/error/ErrorCode.java
@@ -18,7 +18,10 @@ public enum ErrorCode {
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다."),
 
     // 파일 업로드 실패
-    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
+    FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
+    
+    // 
+    PRODUCT_IS_RELEASED(HttpStatus.BAD_REQUEST, "출시된 상품은 삭제할 수 없습니다.");
 
     private HttpStatus status;
     private String message;
