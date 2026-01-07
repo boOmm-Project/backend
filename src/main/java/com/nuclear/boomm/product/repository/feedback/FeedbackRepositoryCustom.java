a9df15f904f4bc4132cf28602f378462d5681558
chaewookim <chaewookim2877@gmail.com>
2026-01-07 11:10:59 +0900

    [refactor] #78 이해관계자 피드백 업데이트 리팩토링
    Feedback 엔티티와 Product 엔티티가 연관되어 있기 때문에 LazyInitializationException 발생 가능. QueryDSL로 해결

--- src/main/java/com/nuclear/boomm/product/repository/feedback/FeedbackRepositoryCustom.java
+++ src/main/java/com/nuclear/boomm/product/repository/feedback/FeedbackRepositoryCustom.java
@@ -3,7 +3,9 @@ package com.nuclear.boomm.product.repository.feedback;
 import com.nuclear.boomm.product.domain.Feedback;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FeedbackRepositoryCustom {
     List<Feedback> searchAllFeedbackByUserIdWithProduct(Long userId);
+    Optional<Feedback> findByFeedbackIdAndWriterId(Long feedbackId, Long userId);
 }
--- src/main/java/com/nuclear/boomm/product/repository/feedback/FeedbackRepositoryImpl.java
+++ src/main/java/com/nuclear/boomm/product/repository/feedback/FeedbackRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.nuclear.boomm.product.domain.QFeedback.feedback;
 import static com.nuclear.boomm.product.domain.QProduct.product;
@@ -21,5 +22,18 @@ public class FeedbackRepositoryImpl implements FeedbackRepositoryCustom {
                 .join(feedback.product, product).fetchJoin()
                 .where(product.userId.eq(userId))
                 .fetch();
+    }
+
+    @Override
+    public Optional<Feedback> findByFeedbackIdAndWriterId(Long feedbackId, Long userId) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(feedback)
+                .join(feedback.product, product).fetchJoin()
+                .where(
+                        feedback.feedbackId.eq(feedbackId),
+                        feedback.writerId.eq(userId)
+                )
+                .fetchOne()
+        );
     }
 }
