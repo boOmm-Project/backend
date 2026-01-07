4e0f5fd16fca6b481d371adcb1087915e6812812
chaewookim <chaewookim2877@gmail.com>
2026-01-07 15:35:31 +0900

    [fix] #78 RiskReport 엔티티 수정
    Product와의 1:1 연관관계 설정, 예측 손해율을 BigDecimal 타입으로 수정

--- src/main/java/com/nuclear/boomm/product/domain/RiskReport.java
+++ src/main/java/com/nuclear/boomm/product/domain/RiskReport.java
@@ -3,20 +3,23 @@ package com.nuclear.boomm.product.domain;
 import com.nuclear.boomm.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Entity
 @Getter
 @Builder
-@Table(name = "risk_report")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class RiskReport extends BaseEntity {
@@ -25,11 +28,12 @@ public class RiskReport extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reportId;
 
-    @Column(nullable = false)
-    private Long productId; // product 테이블 pk 참조
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id",  nullable = false, unique = true)
+    private Product product;
 
-    @Column(nullable = false)
-    private Double lossRatioForecast;
+    @Column(nullable = false, precision = 10, scale = 4)
+    private BigDecimal lossRatioForecast;
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String competitorProductComparison;
