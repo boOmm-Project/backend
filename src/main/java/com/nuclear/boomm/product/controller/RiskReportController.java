549b758411e531d95bb5a4e26d5b7b73517aa2f8
chaewookim <chaewookim2877@gmail.com>
2026-01-07 17:20:35 +0900

    [feat] #84 위험보고서 생성 로직 구현
    Entity 수정, DTO 생성, 서비스 구현

--- src/main/java/com/nuclear/boomm/product/controller/RiskReportController.java
+++ src/main/java/com/nuclear/boomm/product/controller/RiskReportController.java
@@ -1,4 +1,31 @@
 package com.nuclear.boomm.product.controller;
 
+import com.nuclear.boomm.common.ApiResponse;
+import com.nuclear.boomm.product.dto.response.product.RiskReportResponse;
+import com.nuclear.boomm.product.service.RiskReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "위험 보고서", description = "위험 보고서 생성, 수정, 피드백 반영, 삭제 API")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/reports")
+@RestController
 public class RiskReportController {
+    private final RiskReportService riskReportService;
+
+    @Operation(summary = "위험 보고서 생성", description = "상품 관리자가 위험 보고서를 생성")
+    @PostMapping("/{id}")
+    public ResponseEntity<ApiResponse<RiskReportResponse>> createRiskReport(
+            @PathVariable("id") Long productId
+    ) {
+        Long userId = 1L;
+
+        return ResponseEntity.ok(ApiResponse.success(riskReportService.createRiskReport(userId, productId)));
+    }
 }
--- src/main/java/com/nuclear/boomm/product/service/RiskReportService.java
+++ src/main/java/com/nuclear/boomm/product/service/RiskReportService.java
@@ -1,4 +1,46 @@
 package com.nuclear.boomm.product.service;
 
+import com.nuclear.boomm.product.domain.Product;
+import com.nuclear.boomm.product.domain.RiskReport;
+import com.nuclear.boomm.product.dto.response.product.RiskReportResponse;
+import com.nuclear.boomm.product.error.CustomException;
+import com.nuclear.boomm.product.error.ErrorCode;
+import com.nuclear.boomm.product.repository.product.ProductRepository;
+import com.nuclear.boomm.product.repository.product.RiskReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class RiskReportService {
+    private final RiskReportRepository riskReportRepository;
+    private final ProductRepository productRepository;
+
+    /**
+     * 상품에 대한 위험 보고서 생성
+     *
+     * @param userId    상품 관리자 고유 번호
+     * @param productId 상품 고유 번호
+     * @return RiskReportResponse    Dto로 변환된 RiskReport
+     * @throws CustomException(ErrorCode.PRODUCT_NOT_FOUND) 해당 상품이 상품 관리자가 생성한 상품이 아닐 경우, 해당 번호의 상품이 없을 경우
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public RiskReportResponse createRiskReport(Long userId, Long productId) {
+        // 전달받은 productId로 Product 조회
+        Product product = productRepository.findByProductId(productId)
+                .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+
+        // 해당 상품 관리자가 상품의 생성자가 아니라면 예외
+        if (!product.getUserId().equals(userId)) {
+            throw new CustomException(ErrorCode.PRODUCT_NOT_FOUND);
+        }
+
+        // RiskReport 생성 및 저장 후 반환
+        return RiskReportResponse.from(riskReportRepository.save(
+                RiskReport.builder()
+                        .product(product)
+                        .build()
+        ));
+    }
 }
