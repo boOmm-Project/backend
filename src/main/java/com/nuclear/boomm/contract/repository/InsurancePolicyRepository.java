cebb9a9f1df3b5317da603a44033da869916ba60
yoocy0 <yoocy0328@gmail.com>
2026-01-20 20:07:53 +0900

    [refactor] #89 JpaRepository 의 제네릭 타입 ë³수정

--- src/main/java/com/nuclear/boomm/contract/repository/InsurancePolicyRepository.java
+++ src/main/java/com/nuclear/boomm/contract/repository/InsurancePolicyRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InsurancePolicyRepository extends JpaRepository<InsurancePolicy, Long> {
+public interface InsurancePolicyRepository extends JpaRepository<InsurancePolicy, String> {
 
 }
